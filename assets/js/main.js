0357e495139fa17407f0ee23aa47fb0f6622ba80
Michele Milesi <michele.milesi@studio.unibo.it>
2023-05-17 08:21:15 +0000

    fix: font and responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,11 @@
 $(document).ready(function () {
-    previous_width = $("html").width();
-    $("html").on("resize", function () {
-        if ($("html").width() >= 900 && previous_width < 900) {
-            $("code").css("opacity", .4);
-            $("code[data-index='" + $(".feature-wrapper.active").attr("data-index") + "']").css("opacity", 1);
-            previous_width = $("html").width();
-            $(".code-wrapper").show()
-        }
-        if ($("html").width() < 900 && previous_width >= 900) {
-            $(".code-wrapper").hide();
-            $(".code-wrapper[data-index='" + $(".feature-wrapper.active").attr("data-index") + "']").show();
-            previous_width = $("html").width();
-        }
-    });
+    $(".code-wrapper[data-index=0]").addClass("active");
     $(".feature-wrapper").on("mouseover", function () {
         if (!$(this).hasClass("active")) {
-            $(".feature-wrapper.active").removeClass("active");
+            $(".feature-wrapper.active, code.active, .code-wrapper.active").removeClass("active");
             $(this).addClass("active");
-            if ($("html").width() >= 900) {
-                $("code").css("opacity", .4);
-                $("code[data-index='" + $(this).attr("data-index") + "']").css("opacity", 1);
-            }
-            else {
-                $(".code-wrapper").hide();
-                $(".code-wrapper[data-index='" + $(this).attr("data-index") + "']").show();
-            }
-
+            $("code[data-index='" + $(this).attr("data-index") + "']").addClass("active")
+            $(".code-wrapper[data-index='" + $(this).attr("data-index") + "']").addClass("active")
         }
     });
 });
