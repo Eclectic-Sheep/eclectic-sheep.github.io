cd87cd5c31c201f2e98ce836c9ffb896320cc4fa
belerico_t <federico.belotti@orobix.com>
2023-12-14 15:25:31 +0100

    Removed commented function

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,24 +68,6 @@ $(document).ready(function () {
         });
     });
 
-    // Collapsible
-    // var coll = document.getElementsByClassName("collapsible");
-    // var i;
-
-    // for (i = 0; i < coll.length; i++) {
-    //     coll[i].addEventListener("click", function () {
-    //         this.classList.toggle("active");
-    //         var content = this.parentElement.nextElementSibling;
-    //         if (content.style.maxHeight) {
-    //             content.style.maxHeight = null;
-    //             //content.style.display = "none";
-    //         } else {
-    //             content.style.display = "block";
-    //             content.style.maxHeight = content.scrollHeight + "px";
-    //         }
-    //     });
-    // }
-
     $(".collapsible").click(function () {
         $(this).parent().next().slideToggle(500);
     });
