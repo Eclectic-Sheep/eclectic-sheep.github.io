8312df87e6168fd966f94aebc607165d0bd0b2f1
belerico_t <federico.belotti@orobix.com>
2023-12-20 16:31:07 +0100

    Fix a2c post with last devs

--- script.py
+++ script.py
@@ -8,22 +8,20 @@ found in the *.hbs files.
 import os
 import glob
 
-if __name__ == '__main__':
-
-    os.chdir(os.path.join(os.environ['HOME'],
-                          "Downloads", "Casper-master"))
+if __name__ == "__main__":
+    os.chdir(os.path.join(os.environ["HOME"], "Downloads", "Casper-master"))
     for filename in glob.glob("*.hbs"):
         print(filename)
 
         # Read in the file
-        with open(filename, 'r') as infile :
-          filedata = infile.read()
+        with open(filename, "r") as infile:
+            filedata = infile.read()
 
         # Replace the target string
         # Replacing comments
-        filedata = filedata.replace('{{!--', '<!--')
-        filedata = filedata.replace('--}}', '-->')
+        filedata = filedata.replace("{{!--", "<!--")
+        filedata = filedata.replace("--}}", "-->")
 
         # Write the file out again
-        with open(filename, 'w') as outfile:
-          outfile.write(filedata)
+        with open(filename, "w") as outfile:
+            outfile.write(filedata)
